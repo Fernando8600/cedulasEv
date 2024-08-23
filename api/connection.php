c794c246b7b609e61e116f64238c69fa8c3dea0d
Fernando Gutierrez <gutierrezhernandez.f86@gmail.com>
2024-08-22 19:58:30 -0600

    VITE 2

--- api/connection.php
+++ api/connection.php
@@ -4,10 +4,11 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 function createConnection()
 {
-    $servername = "localhost";
-    $username = "root";
-    $password = "ferserv860";
-    $dbname = "cedula_evaluaciones";
+    $servername = "seseq.mx";
+    $username = "root_cedulas";
+    $password = "Cedulas01";
+    $dbname = "u970328549_cedulas";
+
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
